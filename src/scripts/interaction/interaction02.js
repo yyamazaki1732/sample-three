f3cf9361f878fb8cb4f215e3e61f225be7c94785
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-07-15 16:44:54 +0900

    add: scroll function to imteractino02

--- src/scripts/interaction/interaction02.js
+++ src/scripts/interaction/interaction02.js
@@ -6,6 +6,7 @@ function init() {
   const width = window.innerWidth;
   const height = window.innerHeight;
   const mouse = new THREE.Vector2(0, 0);
+  let increment_scrollY = 0;
   // レンダラーを作成
   const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#myCanvas"),
@@ -32,11 +33,12 @@ function init() {
   const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
   // メッシュを作成
   const mesh = new THREE.Mesh(geometry, material);
+
   // 3D空間にメッシュを追加
   scene.add(mesh);
 
   // 平行光源
-  const spotLight = new THREE.SpotLight(0xff00ff);
+  const spotLight = new THREE.SpotLight(0xff000f);
   spotLight.position.set(0, 0, dist);
   scene.add(spotLight);
 
@@ -47,7 +49,6 @@ function init() {
   //   scene.add(pointLightHelper);
 
   //   mousemove
-
   function mouseMoved(x, y) {
     mouse.x = x - width / 2; // 原点を中心に持ってくる
     mouse.y = -y + height / 2; // 軸を反転して原点を中心に持ってくる
@@ -60,16 +61,25 @@ function init() {
     mouseMoved(e.clientX, e.clientY);
   });
 
+  //   scroll
+  function scrolled(y) {
+    increment_scrollY = y;
+  }
+  scrolled(window.scrollY);
+
+  window.addEventListener("scroll", () => {
+    scrolled(window.scrollY);
+  });
+
   tick();
 
   // 毎フレーム時に実行されるループイベントです
   function tick() {
     // メッシュを回転させる
-    // mesh.rotation.x = Math.PI / 4;
-    // mesh.rotation.y = Math.PI / 4;
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.01;
     mesh.rotation.z += 0.01;
+    mesh.position.y = increment_scrollY;
     // ライトを周回させる
     //   pointLight.position.set(
     //     500 * Math.sin(1000 / 500),
