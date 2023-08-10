957cb8dd615f52796f865e610439bd49b7a54b1c
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-08-10 22:51:22 +0900

    wip: ひとまずできた。

--- src/scripts/others/countdown-after.js
+++ src/scripts/others/countdown-after.js
@@ -2,28 +2,80 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
-// ===============================================
-// # handleFadeIn
-// ===============================================
 (() => {
-  const DURARION = 1;
+  const DURARION = 0.8;
+
+  // ===============================================
+  // # fv
+  // ===============================================
+
+  const handleFv = () => {
+    const fv = document.querySelectorAll(".fv_title");
+    ScrollTrigger.create({
+      trigger: fv,
+      start: "center center",
+      end: "100%",
+      scrub: 0,
+      pin: true,
+    });
+  };
+  // ===============================================
+  // # handleScroll
+  // ===============================================
+  const handleScroll = () => {
+    const CONTAINER = document.querySelectorAll(".js-text-box");
+
+    CONTAINER.forEach((element) => {
+      const IMAGE_BOX = element.nextElementSibling;
+      const HEIGHT = IMAGE_BOX.getBoundingClientRect().height;
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: element,
+          start: "center center",
+          end: () => `${HEIGHT}px`,
+          scrub: 0,
+          pin: true,
+          invalidateOnRefresh: true,
+        },
+      });
+
+      tl.scrollTrigger.refresh();
+
+      // ScrollTrigger.create({
+      //   trigger: element,
+      //   start: "center center",
+      //   end: () => `${HEIGHT}px`,
+      //   scrub: 0,
+      //   pin: true,
+      // });
+    });
+  };
+
+  // ===============================================
+  // # handleFadeIn
+  // ===============================================
 
   const handleFadeIn = () => {
     const HANDLE_FADEIN = document.querySelectorAll(".handle-fadein");
-    console.log(HANDLE_FADEIN);
+
     HANDLE_FADEIN.forEach((element) => {
+      const START_POS = element.classList.contains("start-delay")
+        ? "85%" // normal
+        : "65%"; // start-delay
+
       const FadeIn = () => {
         gsap.to(element, {
           translateY: 0,
           opacity: 1,
           duration: DURARION,
-          ease: "Power2.out",
+          ease: "Power4.out",
           overwrite: true,
         });
       };
       const FadeOut = () => {
         gsap.to(element, {
-          translateY: "10px",
+          translateY: "10%",
           opacity: 0,
           overwrite: true,
         });
@@ -32,9 +84,9 @@ gsap.registerPlugin(ScrollTrigger);
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: element,
-          start: "top 60%",
-          ease: "Power4.easeOut",
-
+          start: `top ${START_POS}`,
+          ease: "Power4.ease",
+          invalidateOnRefresh: true,
           onEnter: () => {
             FadeIn();
           },
@@ -43,86 +95,46 @@ gsap.registerPlugin(ScrollTrigger);
           },
         },
       });
+
+      tl.scrollTrigger.refresh();
     });
   };
   // ===============================================
-  // # pin
+  // # handleBg
   // ===============================================
-  const handleScroll = () => {
-    const CONTAINER = document.querySelectorAll(".handle-scroll");
+  const handleBg = () => {
+    const CONTAINER = document.querySelectorAll(".handle-bg");
+    const BG_M = document.getElementsByClassName("js-bg")[0].children[0];
+    const BG_W = document.getElementsByClassName("js-bg")[0].children[1];
 
-    CONTAINER.forEach((element) => {
-      const TEXT_BOX = element.children[0].children[1];
-      const IMAGE_BOX = element.children[0].children[2];
-      const BG = element.children[0].children[3];
-
-      const HEIGHT = IMAGE_BOX.getBoundingClientRect().height;
-      gsap.set(IMAGE_BOX, { translateY: `${HEIGHT}px` });
-      const tl = gsap.timeline({
-        scrollTrigger: {
+    CONTAINER.forEach((element, index) => {
+      const handleLogoMen = () => {
+        ScrollTrigger.create({
           trigger: element,
-          start: "center center",
-          end: "+=400%",
-          scrub: 0.1,
-          pin: true,
-          markers: true,
-        },
-      });
-      tl.fromTo(
-        IMAGE_BOX,
-        {
-          translateY: `${HEIGHT}px`,
-        },
-        {
-          translateY: `-${HEIGHT + 40}px`,
-        }
-      );
-      window.addEventListener("scroll", () => {
-        const START_LINE = window.innerHeight * (3 / 4);
-        const END_LINE = window.innerHeight * (1 / 5);
+          start: "top center",
+          end: "bottom center",
+          // anticipatePin: 1,
+          toggleClass: { targets: BG_M, className: "is-show" },
+        });
+      };
+      const handleLogoWomen = () => {
+        ScrollTrigger.create({
+          trigger: element,
+          start: "top center",
+          end: "bottom center",
+          // anticipatePin: 1,
+          toggleClass: { targets: BG_W, className: "is-show" },
+        });
+      };
 
-        const fadeInFix = () => {
-          gsap.to(TEXT_BOX, {
-            opacity: 1,
-            duration: 0.5,
-            overwrite: true,
-          });
-          gsap.to(BG, {
-            opacity: 1,
-            duration: 0.5,
-            overwrite: true,
-          });
-        };
-        const fadeOutFix = () => {
-          gsap.to(TEXT_BOX, {
-            opacity: 0,
-            duration: 0.5,
-            overwrite: true,
-          });
-          gsap.to(BG, {
-            opacity: 0,
-            duration: 0.5,
-            overwrite: true,
-          });
-        };
-
-        IMAGE_BOX.getBoundingClientRect().top <= START_LINE
-          ? fadeInFix()
-          : IMAGE_BOX.getBoundingClientRect().top > START_LINE
-          ? fadeOutFix()
-          : null;
-
-        IMAGE_BOX.getBoundingClientRect().bottom <= END_LINE
-          ? fadeOutFix()
-          : IMAGE_BOX.getBoundingClientRect().bottom <= END_LINE
-          ? fadeInFix()
-          : null;
-      });
+      index == 0 ? handleLogoMen() : handleLogoWomen();
     });
   };
 
   const setAnimation = () => {
+    // handleFv();
     handleScroll();
+    handleBg();
     handleFadeIn();
   };
 
